8ad800221839ed95323ff5fc21a74d6ad6cf19c6
gongdo <gongdo@live.com>
2017-08-29 13:36:12 +0900

    initial commit

---
+++ LoggingLibraryDummy461/Dummy461.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingLibraryDummy461
+{
+    public class Dummy461
+    {
+    }
+}
